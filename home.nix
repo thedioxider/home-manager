f902f090dee04dc695d6126901fd7e308b8d3c18
Demetrius <thedioxider@mail.ru>
2025-04-30 04:59:02 +0300

    feat(gc): automatic garbage collection

--- home.nix
+++ home.nix
@@ -1,13 +1,17 @@
-{ lib, pkgs, ... }:
-{
+{ lib, pkgs, ... }: {
   home = {
-    packages = with pkgs; [
-      hello
-    ];
+    packages = with pkgs; [ kdePackages.filelight ];
 
     username = "dio";
     homeDirectory = "/home/dio";
 
     stateVersion = "24.11";
+  };
+
+  nix.gc = {
+    automatic = true;
+    frequency = "weekly";
+    persistent = true;
+    options = "--delete-older-than 30d";
   };
 }
