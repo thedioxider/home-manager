9060973ab6fbeca5e51685b8eb0700fbd5e56a1c
Demetrius <thedioxider@mail.ru>
2026-03-11 22:08:06 +0300

    refactor(hypr): remove UWSM, enable XDG autostart

--- hypr/default.nix
+++ hypr/default.nix
@@ -9,9 +9,7 @@
   wayland.windowManager.hyprland = {
     enable = true;
     xwayland.enable = true;
-
-    # use USWM instead
-    systemd.enable = false;
+    systemd.enableXdgAutostart = true;
   };
 
   home.packages = with pkgs; [
