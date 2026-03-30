933eb09b462646f52833b16ec23f8ed25a8bf5bd
Demetrius <thedioxider@mail.ru>
2026-03-30 14:49:26 +0300

    feat(neovim): add diovim config as submodule with globLink

--- neovim.nix
+++ neovim.nix
@@ -1,15 +1,16 @@
-# TODO: modifiable way to fetch the live configs from github
 {
   config,
-  lib,
   pkgs,
   inputs,
+  globLink,
   ...
 }:
 let
   neovim-nightly = inputs.neovim-nightly.packages.${pkgs.stdenv.hostPlatform.system}.default;
 in
 {
+  xdg.configFile.nvim.source = globLink config "config/nvim";
+
   home.file.".nvim".source = config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/nvim";
 
   home.packages = [
