fd67903645c5eb418ee97f3b7e300d27db978752
Demetrius <thedioxider@mail.ru>
2026-05-18 21:25:25 +0300

    feat(hypr): window rule for PiP

--- config/hypr/land/workspace.lua
+++ config/hypr/land/workspace.lua
@@ -83,3 +83,14 @@ hl.window_rule({
 	border_size = 0,
 	rounding = 0,
 })
+
+-- Picture-in-Picture
+hl.window_rule({
+	match = { title = "^Picture-in-Picture$" },
+	float = true,
+	pin = true,
+	content = "video",
+	border_size = 1,
+	keep_aspect_ratio = true,
+	move = { "monitor_w - window_w - 1", "monitor_h - window_h - 1" },
+})
