fb48483484fb919265364ab47656572d4e68f1e5
Demetrius <thedioxider@mail.ru>
2026-05-23 15:32:40 +0300

    feat(hypr): add util to show layers info

--- config/hypr/land/dev_utils.lua
+++ config/hypr/land/dev_utils.lua
@@ -1,11 +1,19 @@
 -- show info about current window
 hl.window_rule({
-	match = { title = "^window_info$" },
+	match = { title = "^hyprinfo$" },
 	float = true,
 })
 hl.bind(
 	MAIN_MOD .. "+F12",
 	hl.dsp.exec_cmd(
-		"hyprctl activewindow > /tmp/window_info.txt && kitty --title window_info sh -c 'cat /tmp/window_info.txt ; read'"
+		"hyprctl activewindow > /tmp/window_info.txt && kitty --title hyprinfo sh -c 'cat /tmp/window_info.txt ; read'"
+	)
+)
+
+-- show info about current layer
+hl.bind(
+	MAIN_MOD .. "+ALT+F12",
+	hl.dsp.exec_cmd(
+		"hyprctl layers > /tmp/layers_info.txt && kitty --title hyprinfo sh -c 'cat /tmp/layers_info.txt ; read'"
 	)
 )
